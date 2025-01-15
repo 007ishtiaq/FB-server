ff11c46f1f9546aecf1b39d5d0e40a4d030d78f8
ishtiaq_ahmad <ishtiaqahmad427@gmail.com>
2025-01-15 09:12:45 +0500

    stripe working

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -3,32 +3,23 @@ const Cart = require("../models/cart");
 const Product = require("../models/product");
 const Coupon = require("../models/coupon");
 const coupon = require("../models/coupon");
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 exports.createPaymentIntent = async (req, res) => {
-  // console.log(req.body);
-  const { couponApplied } = req.body;
+  const { coupon } = req.body;
 
-  // later apply coupon
-  // later calculate price
-
-  // 1 find user
   const user = await User.findOne({ email: req.user.email }).exec();
-  // 2 get user cart total
-  const { cartTotal, totalAfterDiscount } = await Cart.findOne({
+  const { cartTotal, shippingfee, totalAfterDiscount } = await Cart.findOne({
     orderdBy: user._id,
   }).exec();
-  // console.log("CART TOTAL", cartTotal, "AFTER DIS%", totalAfterDiscount);
 
   let finalAmount = 0;
 
-  if (couponApplied && totalAfterDiscount) {
+  if (coupon && coupon.applied && totalAfterDiscount) {
     finalAmount = totalAfterDiscount * 100;
   } else {
-    finalAmount = cartTotal * 100;
+    finalAmount = (cartTotal + shippingfee) * 100;
   }
-
-  // create payment intent with order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: finalAmount,
     currency: "usd",
--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
+const Stripe = require("stripe");
 require("dotenv").config();
-
 // app
 const app = express();
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 // db
 mongoose
