17fd1b78be57d3e92f65e9dcfb494dec354f6cda
ishtiaq_ahmad <ishtiaqahmad427@gmail.com>
2025-01-22 08:02:21 +0500

    stripe user info comment for some time

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -5,11 +5,13 @@ const Coupon = require("../models/coupon");
 const coupon = require("../models/coupon");
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
+//PaymentIntent with only product info
+
 exports.createPaymentIntent = async (req, res) => {
   const { coupon } = req.body;
 
   try {
-    // Fetch user and cart info
+    // Fetch the user and their cart info
     const user = await User.findOne({ email: req.user.email }).exec();
     const cartinfo = await Cart.findOne({ orderdBy: user._id }).exec();
 
@@ -19,7 +21,7 @@ exports.createPaymentIntent = async (req, res) => {
 
     const { cartTotal, shippingfee, totalAfterDiscount, products } = cartinfo;
 
-    // Calculate final amount
+    // Calculate the final amount
     let finalAmount = 0;
     if (coupon && coupon.applied && totalAfterDiscount) {
       finalAmount = totalAfterDiscount * 100;
@@ -41,37 +43,19 @@ exports.createPaymentIntent = async (req, res) => {
       return { ...acc, ...item };
     }, {});
 
-    // Add user-related metadata
-    const userMetadata = {
-      userId: user._id.toString(),
-      email: user.email,
-      name: user.name,
-      contact: user.contact || "N/A",
-      DOB: user.profile?.DOB || "N/A",
-      Gender: user.profile?.Gender || "N/A",
-      Address: user.address?.Address || "N/A",
-      City: user.address?.City || "N/A",
-      Province: user.address?.Province || "N/A",
-      Area: user.address?.Area || "N/A",
-      LandMark: user.address?.LandMark || "N/A",
-    };
-
-    // Combine all metadata
-    const metadata = {
-      ...flatMetadata,
-      ...userMetadata,
-      cartTotal: cartTotal.toString(),
-      shippingfee: shippingfee.toString(),
-      totalAfterDiscount: totalAfterDiscount
-        ? totalAfterDiscount.toString()
-        : "0",
-    };
-
-    // Create payment intent
+    // Create a payment intent with product metadata
     const paymentIntent = await stripe.paymentIntents.create({
       amount: finalAmount,
       currency: "usd",
-      metadata,
+      metadata: {
+        ...flatMetadata,
+        userId: user._id.toString(),
+        cartTotal: cartTotal.toString(),
+        shippingfee: shippingfee.toString(),
+        totalAfterDiscount: totalAfterDiscount
+          ? totalAfterDiscount.toString()
+          : "0",
+      },
     });
 
     // Send the response
@@ -87,3 +71,88 @@ exports.createPaymentIntent = async (req, res) => {
     res.status(500).send({ error: "Failed to create payment intent." });
   }
 };
+
+//PaymentIntent with client info
+
+// exports.createPaymentIntent = async (req, res) => {
+//   const { coupon } = req.body;
+
+//   try {
+//     // Fetch user and cart info
+//     const user = await User.findOne({ email: req.user.email }).exec();
+//     const cartinfo = await Cart.findOne({ orderdBy: user._id }).exec();
+
+//     if (!cartinfo) {
+//       return res.status(400).send({ error: "Cart not found." });
+//     }
+
+//     const { cartTotal, shippingfee, totalAfterDiscount, products } = cartinfo;
+
+//     // Calculate final amount
+//     let finalAmount = 0;
+//     if (coupon && coupon.applied && totalAfterDiscount) {
+//       finalAmount = totalAfterDiscount * 100;
+//     } else {
+//       finalAmount = (cartTotal + shippingfee) * 100;
+//     }
+
+//     // Prepare product metadata
+//     const productMetadata = products.map((product, index) => ({
+//       [`product_${index + 1}_id`]: product.product.toString(),
+//       [`product_${index + 1}_count`]: product.count.toString(),
+//       [`product_${index + 1}_color`]: product.color,
+//       [`product_${index + 1}_size`]: product.size,
+//       [`product_${index + 1}_price`]: product.price.toString(),
+//     }));
+
+//     // Flatten the metadata array for Stripe
+//     const flatMetadata = productMetadata.reduce((acc, item) => {
+//       return { ...acc, ...item };
+//     }, {});
+
+//     // Add user-related metadata
+//     const userMetadata = {
+//       userId: user._id.toString(),
+//       email: user.email,
+//       name: user.name,
+//       contact: user.contact || "N/A",
+//       DOB: user.profile?.DOB || "N/A",
+//       Gender: user.profile?.Gender || "N/A",
+//       Address: user.address?.Address || "N/A",
+//       City: user.address?.City || "N/A",
+//       Province: user.address?.Province || "N/A",
+//       Area: user.address?.Area || "N/A",
+//       LandMark: user.address?.LandMark || "N/A",
+//     };
+
+//     // Combine all metadata
+//     const metadata = {
+//       ...flatMetadata,
+//       ...userMetadata,
+//       cartTotal: cartTotal.toString(),
+//       shippingfee: shippingfee.toString(),
+//       totalAfterDiscount: totalAfterDiscount
+//         ? totalAfterDiscount.toString()
+//         : "0",
+//     };
+
+//     // Create payment intent
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: finalAmount,
+//       currency: "usd",
+//       metadata,
+//     });
+
+//     // Send the response
+//     res.send({
+//       clientSecret: paymentIntent.client_secret,
+//       cartTotal,
+//       totalAfterDiscount,
+//       payable: finalAmount,
+//       shippingfee,
+//     });
+//   } catch (error) {
+//     console.error("Error creating payment intent:", error);
+//     res.status(500).send({ error: "Failed to create payment intent." });
+//   }
+// };
